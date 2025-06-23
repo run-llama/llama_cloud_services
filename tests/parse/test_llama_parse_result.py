1eafe2d988ea3631c6b67b7dd33d960fada68bd3
Neeraj Pradhan <neeraj@runllama.ai>
2025-06-23 11:46:02 -0700

    Bump up version to 0.6.36

--- tests/parse/test_llama_parse_result.py
+++ tests/parse/test_llama_parse_result.py
@@ -104,11 +104,11 @@ async def test_link_parse_result(file_path: str):
     os.environ.get("LLAMA_CLOUD_API_KEY", "") == "",
     reason="LLAMA_CLOUD_API_KEY not set",
 )
-@pytest.mark.skip(reason="TODO: Needs to be fixed in prod. Raising 500 error.")
 async def test_parse_structured_output(file_path: str):
     parser = LlamaParse(
         structured_output=True,
         structured_output_json_schema_name="imFeelingLucky",
+        invalidate_cache=True,
     )
     result = await parser.aparse(file_path)
     assert isinstance(result, JobResult)
