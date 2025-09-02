a0131799ce3775924ef30eac1e88047396d8f28a
Yannis Panagis <124510903+dcg601@users.noreply.github.com>
2025-09-02 15:42:28 +0200

    Added "SourceText" to __init__.py
    Added "SourceText" to __init__.py, in order for the method to be available for imports in the cases we would like to directly provide the text to be used for information extraction, as also mentioned in the documentation (https://docs.cloud.llamaindex.ai/llamaextract/getting_started/python).

--- py/llama_cloud_services/__init__.py
+++ py/llama_cloud_services/__init__.py
@@ -1,6 +1,6 @@
 from llama_cloud_services.parse import LlamaParse
 from llama_cloud_services.report import ReportClient, LlamaReport
-from llama_cloud_services.extract import LlamaExtract, ExtractionAgent
+from llama_cloud_services.extract import LlamaExtract, ExtractionAgent, SourceText
 from llama_cloud_services.constants import EU_BASE_URL
 from llama_cloud_services.index import (
     LlamaCloudCompositeRetriever,
@@ -14,6 +14,7 @@ __all__ = [
     "LlamaReport",
     "LlamaExtract",
     "ExtractionAgent",
+    "SourceText",
     "EU_BASE_URL",
     "LlamaCloudIndex",
     "LlamaCloudRetriever",
